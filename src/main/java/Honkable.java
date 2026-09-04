a00e721892f81689d68b7379b00f41cfec5de6b9
PatrickH1017 <phickeyy@gmail.com>
2026-09-04 14:42:48 -0400

    chore: import starter project

---
+++ src/main/java/Honkable.java
@@ -0,0 +1,12 @@
+/**
+ * Anything with a horn. Nothing to do in this file.
+ * Vehicle implements it in TODO-04.
+ */
+public interface Honkable {
+
+    String hornSound();
+
+    void honk();
+
+    void honk(int times);
+}
